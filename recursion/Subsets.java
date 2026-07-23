14de71fb69e26cfe2f9d85734b9713313c813583
hemanthjangam <hemanthjangam.edu@gmail.com>
2026-07-23 21:40:04 +0530

    added subsets

--- recursion/Subsets.java
+++ recursion/Subsets.java
@@ -6,19 +6,37 @@ import java.util.List;
 public class Subsets {
     static void main(String[] args) {
         int[] nums = {1, 2, 3};
-        System.out.println(generate(nums, 0, new ArrayList<>(), 0));
+//        System.out.println(generate(nums, 0, new ArrayList<>(), 0));
+        System.out.println(generate(nums, 0));
     }
 
-    static List<List<Integer>> generate(int[] nums, int index, List<List<Integer>> output, int outIndex) {
-        if (index >= nums.length) return output;
-        if (output.isEmpty()) output.add(new ArrayList<>());
-        List<List<Integer>> newSubsets = new ArrayList<>();
-        for (List<Integer> subset : output) {
-            List<Integer> subsets = new ArrayList<>(subset);
-            subsets.add(nums[index]);
-            newSubsets.add(subsets);
+//    static List<List<Integer>> generate(int[] nums, int index, List<List<Integer>> output, int outIndex) {
+//        if (index >= nums.length) return output;
+//        if (output.isEmpty()) output.add(new ArrayList<>());
+//        List<List<Integer>> newSubsets = new ArrayList<>();
+//        for (List<Integer> subset : output) {
+//            List<Integer> subsets = new ArrayList<>(subset);
+//            subsets.add(nums[index]);
+//            newSubsets.add(subsets);
+//        }
+//        output.addAll(newSubsets);
+//        return generate(nums, index + 1, output, outIndex + 1);
+//    }
+
+    static List<List<Integer>> generate(int[] nums, int index) {
+        if (index == nums.length) {
+            List<List<Integer>> base = new ArrayList<>();
+            base.add(new ArrayList<>());
+            return base;
         }
-        output.addAll(newSubsets);
-        return generate(nums, index + 1, output, outIndex + 1);
+        List<List<Integer>> subsets = generate(nums, index + 1);
+        List<List<Integer>> output = new ArrayList<>();
+        for (List<Integer> sub : subsets) {
+            List<Integer> curr = new ArrayList<>(sub);
+            curr.add(nums[index]);
+            output.add(curr);
+        }
+        subsets.addAll(output);
+        return subsets;
     }
 }
